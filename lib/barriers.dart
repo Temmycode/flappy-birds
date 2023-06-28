88b519ed1f9ed558ece1e4ddc14fe07319a89358
Akisanya Temiloluwa <tolutech2004@gmail.com>
2023-06-28 17:13:05 +0100

    second commit

---
+++ lib/barriers.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Barrier extends StatelessWidget {
+  final size;
+  const Barrier({super.key, this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: size,
+      decoration: BoxDecoration(
+        color: Colors.green,
+        border: Border.all(
+          width: 10,
+          color: Colors.green.shade800,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
